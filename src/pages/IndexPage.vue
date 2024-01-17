0e436494ec89d2ace9f198cf5258900c9b637092
viksh-64 <vikash400001@gmail.com>
2024-01-17 14:26:05 +0530

    final

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,26 +7,44 @@
         </div>
       </q-card-section>
 
+      
       <q-card-section>
         <div class="row justify-center">
 
-          <div class="col-12">
+          <div class="col-12 q-pb-md">
             <q-input v-model="toDoTask" label="Input Task">
               <template v-slot:append>
-                <q-btn round dense flat icon="add" @click="addItem" ></q-btn>
+                <q-btn round dense flat icon="add" @click="addItem"></q-btn>
               </template>
             </q-input>
           </div>
         </div>
       </q-card-section>
 
-      <q-card-section>
+      <q-separator  />
+      <q-card-section v-if="toDoList.length > 0" >
         <div class="row justify-center" v-for="(item, index) in toDoList" :key="index">
           <div class="col-8 q-py-md">
             {{ item.value }}
           </div>
+
+          <div class="col-auto q-py-md">
+               <q-btn round dense flat icon="done" @click="removeItem(index)"></q-btn>
+          </div>
+
         </div>
       </q-card-section>
+
+      <q-card-section v-else>
+        <div class="row justify-center">
+          <div class="col-12">
+            <div class="text-h6">
+              No item in the List
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+
     </q-card>
   </q-page>
 </template>
@@ -41,6 +59,7 @@ export default defineComponent({
     return {
 
       toDoTask: '',
+  
 
       toDoList: [
         { value: "create an app" },
@@ -50,11 +69,18 @@ export default defineComponent({
     }
   },
 
-  methods:{ 
+  methods: {
 
-    addItem(){
+    addItem() {
 
-      this.toDoList.push({value:this.toDoTask})
+      this.toDoList.push({ value: this.toDoTask })
+
+    },
+
+
+    removeItem(index){
+
+      this.toDoList.splice(index , 1)
        
     }
 
